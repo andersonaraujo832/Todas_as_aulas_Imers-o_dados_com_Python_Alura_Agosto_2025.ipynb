a420526e43b0f063b0581bb2247c8b7f5242668a
Anderson Araujo <aoliveira832@gamil.com>
2025-08-10 18:27:37 -0300

    terceira aula de dados

--- Dados_Aula01.py
+++ Dados_Aula01.py
@@ -1,3 +1,8 @@
+##AULA1 - PALAVRA CHAVE - PANDAS
+##AULA2 - PALAVRA CHAVE - ?
+##AULA3 - PALAVRA CHAVE - ?
+
+
 import pandas as pd
 
 df = pd.read_csv(
@@ -10,7 +15,6 @@ df.shape
 linhas, colunas = df.shape[0], df.shape[1]
 print("linhas:", linhas, "colunas:", colunas)
 
-df.columns
 
 # Dicionário de renomeação
 novos_nomes = {
@@ -32,3 +36,31 @@ df.rename(columns=novos_nomes, inplace=True)
 
 # Verificando resultado
 df.head()
+
+# O método .value_counts() serve para contar quantas vezes cada valor único aparece em uma coluna.
+df["senioridade"].value_counts()
+
+df["contrato"].value_counts()
+
+df["remoto"].value_counts()
+
+df["tamanho_empresa"].value_counts()
+
+senioridade = {"SE": "senior", "MI": "pleno", "EN": "junior", "EX": "executivo"}
+df["senioridade"] = df["senioridade"].replace(senioridade)
+df["senioridade"].value_counts()
+
+contrato = {"FT": "integral", "PT": "parcial", "CT": "contrato", "FL": "freelancer"}
+df["contrato"] = df["contrato"].replace(contrato)
+df["contrato"].value_counts()
+
+tamanho_empresa = {"L": "grande", "S": "pequena", "M": "media"}
+df["tamanho_empresa"] = df["tamanho_empresa"].replace(tamanho_empresa)
+df["tamanho_empresa"].value_counts()
+
+mapa_trabalho = {0: "presencial", 100: "remoto", 50: "hibrido"}
+
+df["remoto"] = df["remoto"].replace(mapa_trabalho)
+df["remoto"].value_counts()
+
+df.describe(include="object")
