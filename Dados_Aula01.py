c2324b6f692924df4fc7379869e3bcdc321b03a0
Anderson Araujo <aoliveira832@gamil.com>
2025-08-11 18:32:36 -0300

    Aula 4 de dados Alura

--- Dados_Aula01.py
+++ Dados_Aula01.py
@@ -1,9 +1,10 @@
 ##AULA1 - PALAVRA CHAVE - PANDAS
-##AULA2 - PALAVRA CHAVE - ?
-##AULA3 - PALAVRA CHAVE - ?
-
+##AULA2 - PALAVRA CHAVE - PRINT
+##AULA3 - PALAVRA CHAVE - MATPLOTLIB
+##AULA4 - PALAVRA CHAVE -
 
 import pandas as pd
+import pip
 
 df = pd.read_csv(
     "https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv"
@@ -64,3 +65,118 @@ df["remoto"] = df["remoto"].replace(mapa_trabalho)
 df["remoto"].value_counts()
 
 df.describe(include="object")
+
+df.isnull().sum()
+
+import numpy as np
+
+# criando dataframe de teste
+df_salarios = pd.DataFrame(
+    {
+        "nome": ["Ana", "Bruno", "Claudio", "João"],
+        "salario": [4000, np.nan, 5000, np.nan],
+    }
+)
+
+# cálcula média salarial e arredonda os valores
+df_salarios["salario_media"] = df_salarios["salario"].fillna(
+    round(df_salarios["salario"].mean(), 2)
+)
+
+df_salarios
+
+
+df_temparaturas = pd.DataFrame(
+    {
+        "Dia": ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"],
+        "Temperatura": [30, np.nan, np.nan, 28, 27],
+    }
+)
+
+df_temparaturas["Preenchido_ffill"] = df_temparaturas["Temperatura"].ffill()
+df_temparaturas
+
+df_cidades = pd.DataFrame(
+    {
+        "nome": ["Ana", "Bruno", "Claudio", "João", "Junior"],
+        "cidade": ["São Paulo", np.nan, "Curitiba", np.nan, "Belém"],
+    }
+)
+
+df_cidades["cidade_preenchida"] = df_cidades["cidade"].fillna("Não Informado")
+df_cidades
+
+df_limpo = df.dropna()
+
+df_limpo.isnull().sum()
+
+df_limpo.head()
+
+df_limpo = df_limpo.assign(ano=df_limpo["ano"].astype("int64"))
+df_limpo.info()
+
+df_limpo["senioridade"].value_counts().plot(
+    kind="bar", title="Distribuição de Senioridade"
+)
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.barplot(data=df_limpo, x="senioridade", y="usd")
+plt.show()
+
+plt.figure(figsize=(8, 5))
+sns.barplot(data=df_limpo, x="senioridade", y="usd")
+plt.title("Salario Medio por Senioridade")
+plt.xlabel("Nível de Senioridade")
+plt.ylabel("Salario Médio Anual")
+plt.show()
+
+ordem = df_limpo.groupby("senioridade")["usd"].mean().sort_values(ascending=False).index
+ordem
+
+plt.figure(figsize=(8, 5))
+sns.barplot(data=df_limpo, x="senioridade", y="usd", order=ordem)
+plt.title("Salario Medio por Senioridade")
+plt.xlabel("Nível de Senioridade")
+plt.ylabel("Salario Médio Anual")
+plt.show()
+
+plt.figure(figsize=(8, 4))
+sns.histplot(df_limpo["usd"], bins=50, kde=True)
+plt.title("Distribuição Salario")
+plt.xlabel("Salario")
+plt.ylabel("Frequencia")
+plt.show()
+
+import streamlit as st
+import plotly.express as px
+import pycountry
+
+
+# Função para converter ISO-2 para ISO-3
+def iso2_to_iso3(code):
+    try:
+        return pycountry.countries.get(alpha_2=code).alpha3
+    except:
+        return None
+
+
+# Criar nova coluna com código ISO3-3
+df_limpo["residencia_iso3"] = df_limpo["residencia"].apply(iso2_to_iso3)
+
+# Calcular média salarial por país (ISO-3)
+df_ds = df_limpo[df_limpo["cargo"] == "Data Scientist"]
+media_ds_pais = df_ds.groupby("residencia_iso3")["usd"].mean().reset_index()
+
+# Gerar o mapa
+fig = px.choroleth(
+    media_ds_pais,
+    locations="residencia_iso3",
+    color="usd",
+    color_continuos_scale="rdylgn",
+    title="Salário médio de Cientista de dados por país",
+    labels={"usd": "Salário médio (USD)", "residencia_iso3": "País"},
+)
+
+fig.show()
